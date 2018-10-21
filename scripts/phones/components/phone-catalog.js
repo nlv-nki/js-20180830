2372187d21f503e70b81377de4b7e4dc1eaa1870
Nikolai <nlv.nki@gmail.com>
2018-10-22 00:52:56 +0300

    map method was changed to foreach

--- scripts/phones/components/phone-catalog.js
+++ scripts/phones/components/phone-catalog.js
@@ -23,31 +23,31 @@ export default class PhoneCatalog extends Component {
 
     this._onPhoneSelected(phoneElement.dataset.phoneId)
   }
+  
+    _render() {
+      let list = document.createElement('ul')
 
-  _render() {
-    this._element.innerHTML = `
-      <ul class="phones">
-        ${ this._phones.map(phone => `
-          <li
-            class="thumbnail"
-            data-element="phone"
-            data-phone-id="${ phone.id }"
-          >
-            <a href="#!/phones/${ phone.id }" class="thumb">
-              <img alt="${ phone.name }" src="${ phone.imageUrl }">
-            </a>
-  
-            <div class="phones__btn-buy-wrapper">
-              <a class="btn btn-success">
-                Add
-              </a>
-            </div>
-  
-            <a href="#!/phones/${ phone.id }">${ phone.name }</a>
-            <p>${ phone.snippet }</p>
-          </li>
-        `).join('') }
-      </ul>
-    `;
-  }
+      this._phones.forEach(phone => {
+        list.innerHTML += `
+       <li
+         style="height:120px"
+         class="thumbnail"
+         data-element="phone"
+         data-phone-id="${ phone.id }"
+        >
+         <a href="#!/phones/${ phone.id }" class="thumb" style="margin: 5px 10px;">
+           <img alt="${ phone.name }" src="${ phone.imageUrl }">
+         </a>
+         <div class="phones__btn-buy-wrapper">
+           <a class="btn btn-success">
+             Add
+           </a>
+         </div>
+         <a href="#!/phones/${ phone.id }">${ phone.name }</a>
+         <p>${ phone.snippet }</p>
+       </li>
+        `
+      });
+      this._element.append(list)
+    }
 }
